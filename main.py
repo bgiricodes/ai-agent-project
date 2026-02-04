8f4d27301247a31cdb6b740f3f0e6eb03bbae732
bgiricodes <bhupes@proton.me>
2026-02-04 09:07:35 -0700

    Initial commit: test-agent complete code

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+#from langchain_openai import ChatOpenAI
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
+import google.generativeai as genai
+from langsmith import traceable
+
+load_dotenv()
+#@traceable
+def main():
+    print("Hello from langchain-course!")
+
+    information = """
+The German Shepherd, also known in Britain as an Alsatian, is a German breed of working dog of medium to large size.
+    """
+    information1 = """
+
+The OSI (Open Systems Interconnection) reference model is a standardized framework developed by ISO to describe how data is transmitted across a network in seven distinct layers, each with specific roles. It helps in designing, understanding, and troubleshooting network systems by isolating functions.
+    """    
+    summary_template=""" 
+given the information {information} about the german shepard dog i want you to explain:
+1. It's body structrue
+2. life span.
+    """
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    llm = ChatOllama(temperature=0, model="gemma3:latest")
+    chain= summary_prompt_template | llm
+    response= chain.invoke(input={"information":information})
+    print(response.content)
+
+
+
+if __name__ == "__main__":
+    main()
