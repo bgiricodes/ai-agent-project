6a81c2077dcef9a17e09c7c0adfb67077f80e186
bgiricodes <bhupes@proton.me>
2026-02-04 09:23:26 -0700

    removed os

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 import google.generativeai as genai
